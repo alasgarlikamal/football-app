dd1eb041b256951f7fa6a9af74fdd295d38d94df
Kamal <elesgerlikamal04@gmail.com>
2024-05-23 12:42:27 +0400

    add: league id to type

--- src/api/types.ts
+++ src/api/types.ts
@@ -2,6 +2,7 @@ export type TeamData = {
   team_name: string;
   team_badge: string;
   league_name: string;
+  league_id: string;
   overall_league_position: string;
   overall_league_payed: string;
   overall_league_W: string;
