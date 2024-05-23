2d95d5c68daa48f6ad3ee8fa4411beda282e558b
Kamal <elesgerlikamal04@gmail.com>
2024-05-23 11:46:28 +0400

    add: league data response type

---
+++ src/api/types.ts
@@ -0,0 +1,14 @@
+export type TeamData = {
+  team_name: string;
+  team_badge: string;
+  league_name: string;
+  overall_league_position: string;
+  overall_league_payed: string;
+  overall_league_W: string;
+  overall_league_D: string;
+  overall_league_L: string;
+  overall_league_PTS: string;
+  overall_league_GF: string;
+  overall_league_GA: string;
+  overall_promotion: string;
+};
